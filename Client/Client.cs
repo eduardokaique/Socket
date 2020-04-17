fadfe0198505f2f9486437e3a67f004568742527
eduardokaique <eduardokaique.dev@gmail.com>
2020-04-17 16:29:07 -0300

    Add project files.

---
+++ Client/Client.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace PortariaRemota
+{
+    public class Client
+    {
+        private static Socket _clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private static byte[] gl_RecTCPBuff = new byte[9];
+        private static System.Timers.Timer gl_Timer = new System.Timers.Timer();
+        static void Main(string[] args)
+        {
+            LoopConnect();
+            //Comando para envio de bytes para o socket
+            //Send();
+            Console.ReadLine();
+        }
+
+        private static void Send()
+        {
+            byte[] command = new byte[] {
+                0x00,
+                0x0C
+            };
+
+            _clientSocket.Blocking = false;
+            
+            _clientSocket.Send(command, command.Length, 0);
+
+            Thread.Sleep(100);
+
+            int _receive = _clientSocket.Receive(gl_RecTCPBuff);
+            //int _receive = 9;
+
+            byte[] data = new byte[_receive];
+                //Array.Copy(gl_RecTCPBuff, data, _receive);
+                byte[] l_frameHex = new byte[_receive];
+
+                Buffer.BlockCopy(gl_RecTCPBuff, 0, data, 0, _receive);
+
+            _clientSocket.Shutdown(SocketShutdown.Both);
+
+            Console.WriteLine(data);
+        }
+
+        private static void LoopConnect()
+        {
+
+            int _tentativas = 0;
+            _clientSocket.Blocking = false;
+            while (!_clientSocket.Connected)
+            {
+                try
+                {
+                    _tentativas++;
+                    _clientSocket.Connect("localhost", 100);
+                    AsyncCallback onconnect = new AsyncCallback(OnConnect);
+
+
+                }
+                catch (SocketException e)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Tentativas de conexão: " + _tentativas.ToString());
+                    Console.WriteLine("Erro:  " + e.Message);
+                }
+            }
+
+            Console.Clear();
+            Console.WriteLine("Cliente Conectado.");
+        }
+
+        public static void OnConnect(IAsyncResult ar)
+        {
+            Socket sock = (Socket)ar.AsyncState;
+
+            if (sock.Connected)
+            {
+                SetupReceiveCallback(sock);
+            }
+        }
+
+        public static void SetupReceiveCallback(Socket sock)
+        {
+            AsyncCallback receiveData = new AsyncCallback(OnReceiveData);
+            IAsyncResult _result = sock.BeginReceive(gl_RecTCPBuff, 0, gl_RecTCPBuff.Length,
+                SocketFlags.None, receiveData, sock);
+        }
+
+        public static void OnReceiveData(IAsyncResult ar)
+        {
+            Socket sock = (Socket)ar.AsyncState;
+
+            if (!sock.Connected) return;
+
+            int l_nBytesRecTCP = sock.EndReceive(ar);
+
+            if (l_nBytesRecTCP > 0)
+            {
+                SetupReceiveCallback(sock);
+
+            }
+            else
+            {
+                sock.Shutdown(SocketShutdown.Both);
+                sock.Close();
+            }
+        }
+
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace PortariaRemota
+{
+    class Server
+    {
+
+        private static Socket csTCP = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private static List<Socket> _clientsSockets = new List<Socket>();
+        private static byte[] gl_RecTCPBuff = new byte[9];
+        private static System.Timers.Timer gl_Timer = new System.Timers.Timer();
+        private static int counter = 0;
+
+        static void Main(string[] args)
+        {
+            SetupServer();
+            Console.ReadLine();
+        }
+        private static void SetupServer()
+        {
+            try
+            {
+                Console.WriteLine("Iniciando servidor...");
+
+                csTCP.Bind(new IPEndPoint(IPAddress.Any, 100));
+                csTCP.Listen(100);
+                while(true)
+                {
+                    csTCP.BeginAccept(new AsyncCallback(AcceptCallBack), null);
+                }
+            }
+
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        private static void AcceptCallBack(IAsyncResult ar)
+        {
+            Socket _socket = csTCP.EndAccept(ar);
+            _clientsSockets.Add(_socket);
+
+            IAsyncResult _result = _socket.BeginReceive(gl_RecTCPBuff, 0, gl_RecTCPBuff.Length, SocketFlags.None, new AsyncCallback(ReceiveCallBack), _socket);
+
+            counter++;
+                Console.Clear();
+                Console.WriteLine($"Clientes conectados: {counter}");
+        }
+        private static void ReceiveCallBack(IAsyncResult ar)
+        {
+            Socket _socket = (Socket)ar.AsyncState;
+            int received = _socket.EndReceive(ar);
+
+            byte[] dataBuf = new byte[received];
+            Array.Copy(gl_RecTCPBuff, dataBuf, received);
+
+            string _texto = Encoding.ASCII.GetString(dataBuf);
+            //Console.WriteLine("Texto recebido" + _texto);
+
+            string response = string.Empty;
+
+            if (_texto.ToLower() != "get time")
+            {
+                response = "Requisição inválida";
+            }
+            else
+            {
+                response = DateTime.Now.ToLongTimeString();
+            }
+
+            byte[] data = Encoding.ASCII.GetBytes(response);
+            _socket.BeginSend(data, 0, data.Length, SocketFlags.None, new AsyncCallback(SendCallBack), _socket);
+        }
+        private static void SendCallBack(IAsyncResult ar)
+        {
+            Socket _socket = (Socket)ar.AsyncState;
+            _socket.EndSend(ar);
+
+        }
+    }
+}
